9d7740a79c764d8c007081b1eb6a40829de587c2
Robert Grutza <robertgrutza@gmail.com>
2013-07-17 02:35:50 +0000

    grader that handles URLS

--- grader.js
+++ grader.js
@@ -1,31 +1,14 @@
 #!/usr/bin/env node
-/*
-Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
 
-References:
-
- + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
-
- + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
-
- + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
-*/
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest    = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://protected-hollows-2561.herokuapp.com/";
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +18,31 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+
+/*
+var webpage_fetch = function(response,status){
+    console.log(response);
+} 
+
+
+rest.get(url).on('complete', webpage_fetch);
+console.log("This prints during fetch.");
+*/
+
+var getResponse = function(url){
+
+    var result = rest.get(url).on('complete', function(response){
+	response;
+    });
+    var instr = result.toString(); 
+};
+/*
+var processResponse(data){
+    var instr = data.toString();
+    return instr;
+};
+*/
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -64,6 +72,7 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url ', 'Path to URL', clone(getResponse), URL_DEFAULT)   
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
